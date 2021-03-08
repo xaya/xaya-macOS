6227879bbf6bceccb4f50b6c9bac6edd428cbd48
vitalyster <vitalyster@gmail.com>
2021-03-08 09:31:28 +0300

    build gloox with correct options (#1)
    * --disable-getaddrinfo required under Mac

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 brew tap vitalyster/homebrew-header-only
-brew install cmake autoconf autoconf-archive automake libtool libjson-rpc-cpp jsoncpp cppzmq lmdb glog gflags googletest protobuf openssl sqlite gloox
+brew uninstall gloox
+brew install cmake autoconf autoconf-archive automake libtool libjson-rpc-cpp jsoncpp cppzmq lmdb glog gflags googletest protobuf openssl sqlite
 cmake .
 make
