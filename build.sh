31586e4835ac38b5fb12170b800ae54dc7e17c2b
vitalyster <vitalyster@gmail.com>
2021-03-08 19:03:18 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 brew tap vitalyster/homebrew-header-only
-brew uninstall gloox
 brew install cmake autoconf autoconf-archive automake libtool libjson-rpc-cpp jsoncpp cppzmq lmdb glog gflags googletest protobuf openssl sqlite
 cmake .
 make
